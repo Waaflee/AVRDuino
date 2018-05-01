92c2e459fe458a171d076583f63ad57fdd7fe1cf
Waaflee <agustinwafle@gmail.com>
2018-05-01 20:19:29 -0300

    more legible code

--- core.c
+++ core.c
@@ -1,9 +1,4 @@
 #include "core.h"
-// int pinout[] = {DDD0, DDD1, DDD2, DDD3, DDD4, DDD5, DDD6,
-//                 DDD7, DDB0, DDB1, DDB2, DDB3, DDB4, DDB5};
-//
-// int pinD[] = {PORTD0, PORTD1, PORTD2, PORTD3, PORTD4, PORTD5, PORTD6,
-//               PORTD7, PORTB0, PORTB1, PORTB2, PORTB3, PORTB4, PORTB5};
 
 uint8_t pinIn[] = {PIND0, PIND1, PIND2, PIND3, PIND4, PIND5, PIND6,
                    PIND7, PINB0, PINB1, PINB2, PINB3, PINB4, PINB5};
@@ -11,6 +6,7 @@ uint8_t pinIn[] = {PIND0, PIND1, PIND2, PIND3, PIND4, PIND5, PIND6,
 int pinout[] = {0, 1, 2, 3, 4,    5,    6,    7,    0,    1,
                 2, 3, 4, 5, DDC0, DDC1, DDC2, DDC3, DDC4, DDC5};
 int pinD[] = {0, 1, 2, 3, 4, 5, 6, 7, 0, 1, 2, 3, 4, 5};
+
 int pinA[] = {PORTC0, PORTC1, PORTC2, PORTC3, PORTC4, PORTC5};
 
 int *getPort(int pin) {
